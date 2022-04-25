a1c40f65c301d3cde12b0a81088d6aefda9a9678
evertcolombia <evertescalante@gmail.com>
2022-04-24 19:58:30 -0500

    add endpoint to get rows from table_name

--- app/api/endpoints/pdfs.py
+++ app/api/endpoints/pdfs.py
@@ -2,6 +2,7 @@ import imp
 from fastapi import APIRouter, Depends, HTTPException
 from pdf2image import convert_from_path
 from sqlmodel import Session
+from sqlalchemy import select, desc
 
 # local imports
 from api.helpers.post_helpers import *
@@ -39,6 +40,7 @@ async def extract_data(
         "End_Date": "",
     }
 
+    
     # extract text from image
     extract = [read_img(images[0])]
     # get and format the data for the first part os res
@@ -46,20 +48,39 @@ async def extract_data(
     # get and format the data for the comments part
     obj = format_comments(regex[1], extract[0], obj)
     obj["Doc_Path"] = doc_path
-    
-    db_obj = ExtractCreate.from_orm(obj)
-    session.add(db_obj)
-    session.commit()
-    session.refresh(db_obj)
 
-    
+    db_obj = ExtractCreate(
+        vendor_name=obj["Vendor_Name"],
+        fiscal_number=obj["Fiscal_Number"],
+        contract=obj["Contract"],
+        start_date=obj["Start_Date"],
+        end_date=obj["End_Date"],
+        comments=obj["Comments"],
+        doc_path=obj["Doc_Path"]
+    )
+
+    try:
+        session.add(db_obj)
+        session.commit()
+        session.refresh(db_obj)
+    except:
+        raise HTTPException(status_code=500, detail="can't save o new extract on db")
+
     res = [
-        [db_obj.id, True],
+        [True, db_obj.id],
         obj
     ]
     return res
 
 
+@router.get("/db_data", response_model=list)
+async def get_data(
+    *,
+    session: Session = Depends(get_session),
+    table_name: str = None
+):
+    if table_name is None:
+        raise HTTPException(status_code=400, detail="table_name is required")
 
-
-        
+    result = session.execute(select(ExtractCreate).order_by(desc(ExtractCreate.id)))
+    return [e for e in result]
--- app/api/models/pdfs.py
+++ app/api/models/pdfs.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel, String, create_engine
 
-class ExtractCreate(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+
+class ExtractBase(SQLModel):
     vendor_name: Optional[str] = Field(default=None)
     fiscal_number: Optional[str] = Field(default=None)
     contract: Optional[str] = Field(default=None)
@@ -10,3 +10,7 @@ class ExtractCreate(SQLModel, table=True):
     end_date: Optional[str] = Field(default=None)
     comments: Optional[str] = Field(default=None)
     doc_path: Optional[str] = Field(default=None)
+
+    
+class ExtractCreate(ExtractBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
